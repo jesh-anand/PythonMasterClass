fe48c5047e4206e18eb0340eac6735d4eec83ddc
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2016-12-27 22:55:30 +0800

    Added string formatting and function

---
+++ section_6--Types/string_format.py
@@ -0,0 +1,3 @@
+number = [4, 5, 6]
+newstring = "Numbers {0}{1}{2}".format(number[0], number[1], number[2])
+print(newstring)
---
+++ section_6--Types/string_functions.py
@@ -0,0 +1,7 @@
+print(",".join(["Apple", "Mango", "Pineapple"]))
+
+print("Hello There".replace("There", "World"))
+
+oldString = "Hello Prajesh"
+newString = oldString.replace("Prajesh", "dmL")
+print(newString)
