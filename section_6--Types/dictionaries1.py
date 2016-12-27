d7242edc6c5b814aeae80b6d3e23f52f2843dadc
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2016-12-27 22:21:31 +0800

    Added dictionary

---
+++ section_6--Types/dictionaries1.py
@@ -0,0 +1,4 @@
+people = {"John": 32, "Rob": 34}
+
+# To retrive value from key
+print(people["John"])
---
+++ section_6--Types/dictionary_functions.py
@@ -0,0 +1,11 @@
+numbers = {
+    1: "one",
+    2: "two",
+    3: "three"
+}
+
+# Check key exist in the dictionary
+print(1 in numbers)
+
+print(numbers.get(4, "Value not found!"))
+
