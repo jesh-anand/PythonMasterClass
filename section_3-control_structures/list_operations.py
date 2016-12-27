56623e1f0bb09c9b55ab0ebd20617bfe408bf03b
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2016-12-27 16:35:10 +0800

    Restructure files

---
+++ section_3-control_structures/list_operations.py
@@ -0,0 +1,9 @@
+# -- Prints the values 15 times
+numbers = [1, 1, 1, 1, 1]
+print(numbers * 3)
+
+# -- Check if apple exist in list
+fruits = ["Apple", "Mango", "Peach"]
+print("Apple" in fruits)
+if "Apple" in fruits:
+    print("Apple is inside fruits!")
