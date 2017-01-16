3e4b5a7cfaacdddc9a5592ac0a43a7500897f881
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2017-01-17 00:03:06 +0100

    Added while loop

---
+++ section_3-control_structures/while_loop.py
@@ -0,0 +1,14 @@
+'''while_loop.py: A while loop script'''
+
+def print_10_times():
+    counter = 0
+    while (counter <= 10):
+        print(counter)
+        counter += 1
+
+def main():
+    print_10_times()
+
+
+if __name__ == '__main__':
+    main()
