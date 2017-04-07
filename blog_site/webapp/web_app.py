8de9bdcb17eb15d5b9de6b79d0d637fc1f885014
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2017-04-07 18:02:03 +0800

    Initial styling

--- blog_site/webapp/web_app.py
+++ blog_site/webapp/web_app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_method():
-    return 'Hello World'
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8660)
