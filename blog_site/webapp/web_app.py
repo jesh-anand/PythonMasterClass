27910de4ee660fa93fcd43cf665a764db7d030c1
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2017-04-12 08:45:02 +0800

    WIP: User login page

--- blog_site/webapp/web_app.py
+++ blog_site/webapp/web_app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import session
+
+from blog_site.webapp.models.user import User
 
 app = Flask(__name__)
 
@@ -7,6 +11,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_method():
     return render_template("login.html")
+
+
+# TODO: To get the profile page to render email address
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+
+    return render_template("profile.html", email=session["email"])
 
 
 if __name__ == '__main__':
